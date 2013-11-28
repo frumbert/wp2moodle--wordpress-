6bdbf48b002b2445c51ede2b5302868323459aa6
Tim St.Clair <tim.stclair@gmail.com>
2013-11-28 19:58:30 +1100

    Added support for groups; fixed timestamp issue

--- wp2m.php
+++ wp2m.php
@@ -137,8 +137,10 @@ function wp2moodle_handler( $atts, $content = null ) {
 	// clone attribs over any default values, builds variables out of them so we can use them below
 	// $class => css class to put on link we build
 	// $cohort => text id of the moodle cohort in which to enrol this user
+	// $group => text id of the moodle group in which to enrol this user
 	extract(shortcode_atts(array(
 		"cohort" => '',
+		"group" => '',
 		"class" => 'wp2moodle',
 		"target" => '_self'
 	), $atts));
@@ -151,7 +153,7 @@ function wp2moodle_handler( $atts, $content = null ) {
 		// encryption = 3des using shared_secret
 		$details = http_build_query(array(
 			"a", rand(1, 1500),									// set first to randomise the encryption when this string is encoded
-			"stamp" => time(),	// unix timestamp so we can check that the link isn't expired
+			"stamp" => time(),									// unix timestamp so we can check that the link isn't expired
 			"firstname" => $current_user->user_firstname,		// first name
 			"lastname" => $current_user->user_lastname,			// last name
 			"email" => $current_user->user_email,				// email
@@ -159,6 +161,7 @@ function wp2moodle_handler( $atts, $content = null ) {
 			"passwordhash" => $current_user->user_pass,			// hash of password (we don't know/care about the raw password)
 			"idnumber" => $current_user->ID,					// int id of user in this db (for user matching on services, etc)
 			"cohort" => $cohort,								// string containing cohort to enrol this user into
+			"group" => $group,									// string containing group to enrol this user into
 			"z" => rand(1, 1500),								// extra randomiser for when this string is encrypted (for variance)
 		));
 		$url = '<a target="'.esc_attr($target).'" class="'.esc_attr($class).'" href="'.get_option('wp2m_moodle_url').WP2M_MOODLE_PLUGIN_URL.encrypt_string($details, get_option('wp2m_shared_secret')).'">'.do_shortcode($content).'</a>';
